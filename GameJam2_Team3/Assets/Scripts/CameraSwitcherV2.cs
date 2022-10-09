100f003d6c3cd6a8b82f59a677b96e9eaff2c25c
rabbit-isdead <70164577+m-hops@users.noreply.github.com>
2022-10-08 22:20:58 -0400

    CameraSwitcherV2 creation
    Added to art test scene

---
+++ GameJam2_Team3/Assets/Scripts/CameraSwitcherV2.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitcherV2 : MonoBehaviour
+{
+    // Variables
+    public GameObject[] Cameras;
+
+    int currentCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentCam = 0;
+        setCam(currentCam);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check Input
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            toggleCam();
+        }
+    }
+
+    // Switches Cameras in array between tru and false state
+    public void setCam(int idx)
+    {
+        for (int i = 0; i < Cameras.Length; i++)
+        {
+            //Grabs next Camera and labels as true
+            if (i == idx)
+            {
+                Cameras[i].SetActive(true);
+            }
+            else
+            //Sets all other cameras to false
+            {
+                Cameras[i].SetActive(false);
+            }
+        }
+    }
+
+    //Triggers camera switch in array
+    public void toggleCam()
+    {
+        currentCam++;
+
+        //Resets camera array number back to 0 at the end of the array
+        if (currentCam > Cameras.Length - 1)
+            currentCam = 0;
+        setCam(currentCam);
+    }
+}
