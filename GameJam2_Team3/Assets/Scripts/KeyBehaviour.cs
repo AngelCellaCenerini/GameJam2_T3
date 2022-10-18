deeafa059990dc8f70664d47648820fcad93154d
Angel CC <angelcenerini@gmail.com>
2022-10-18 18:34:14 -0400

    jakdzkj

--- GameJam2_Team3/Assets/Scripts/KeyBehaviour.cs
+++ GameJam2_Team3/Assets/Scripts/KeyBehaviour.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[RequireComponent(typeof(AudioSource))]
 
 public class KeyBehaviour : MonoBehaviour
 {
     // External References
+    public AudioClip Collectible_SFX;
+    AudioSource CollectedSFX;
+
+    void Start()
+    {
+        CollectedSFX = GetComponent<AudioSource>();
+    }
+
     public void OnMouseDown()
     {
-        // Register Mouse Input
-        Debug.Log("clicked!");
         // Collect Object
+        CollectedSFX.PlayOneShot(Collectible_SFX, 0.5f);
         gameObject.SetActive(false);
-
     }
 
 }
