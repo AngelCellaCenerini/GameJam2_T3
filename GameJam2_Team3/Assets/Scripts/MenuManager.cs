f0f99bcb147b504e9acc4abb35ab3bd1381fd8fd
rabbit-isdead <70164577+m-hops@users.noreply.github.com>
2022-10-23 21:06:55 -0400

    Video intro now loading

--- GameJam2_Team3/Assets/Scripts/MenuManager.cs
+++ GameJam2_Team3/Assets/Scripts/MenuManager.cs
@@ -2,32 +2,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class MenuManager : MonoBehaviour
 {
     [SerializeField] GameObject Main_Menu;
     [SerializeField] GameObject Options_Menu;
+    [SerializeField] GameObject IntroVid_Menu;
+    [SerializeField] VideoPlayer Video;
+    [SerializeField] GameObject MainMenuBKGVid;
 
     private void Start()
     {
         Options_Menu.SetActive(false);
+        IntroVid_Menu.SetActive(false);
+        Video.Prepare();
     }
 
     public void PlayGame()
     {
-        SceneManager.LoadScene("GameplayScene");
+        MainMenuBKGVid.SetActive(false);
+
+        LoadIntroVidMenu();
+
+        Cursor.visible = false;
+        Video.Play();
+
+        Invoke("VideoEnd", 32.0f);
+    }
+
+    void VideoEnd()
+    {
+        Cursor.visible = true;
+        SceneManager.LoadScene(1);
     }
 
     public void LoadOptionsMenu()
     {
         Options_Menu.SetActive(true);
         Main_Menu.SetActive(false);
+        IntroVid_Menu.SetActive(false);
     }
 
     public void LoadMainMenu()
     {
         Options_Menu.SetActive(false);
         Main_Menu.SetActive(true);
+        IntroVid_Menu.SetActive(false);
+    }
+
+    private void LoadIntroVidMenu()
+    {
+        Options_Menu.SetActive(false);
+        Main_Menu.SetActive(false);
+        IntroVid_Menu.SetActive(true);
+
+
     }
 
     public void QuitGame()
