e2440703c3e1bf978a92aa6796f886f024b533d7
Angel CC <angelcenerini@gmail.com>
2022-10-25 01:45:30 -0400

    Picking up finalized
    sound is iffy rn

--- GameJam2_Team3/Assets/Scripts/KeyBehaviour.cs
+++ GameJam2_Team3/Assets/Scripts/KeyBehaviour.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 [RequireComponent(typeof(AudioSource))]
 
 public class KeyBehaviour : MonoBehaviour
@@ -10,18 +11,56 @@ public class KeyBehaviour : MonoBehaviour
     AudioSource CollectedSFX;
     public bool isSelected;
 
-    void Start()
+    // Key Object
+    public Transform Collectible;
+    public GameObject key;
+
+    // NPC
+    public GameObject npc;
+    public NavMeshAgent navMeshAgent;
+
+    void Awake()
     {
+        // Access NPC
+        navMeshAgent = npc.GetComponent<NavMeshAgent>();
+        // Load SFX
         CollectedSFX = GetComponent<AudioSource>();
+        // Default Status
         isSelected = false;
+    }
+
+   void Update()
+    {
+        // Check if object is selected
+        if (isSelected)
+        {
+            navMeshAgent.SetDestination(Collectible.position);
+            StartCoroutine(ExecuteAfterTime(2));
+        }
     }
 
     public void OnMouseDown()
     {
         // Collect Object
-        CollectedSFX.PlayOneShot(Collectible_SFX, 0.5f);
         isSelected = true;
-
     }
 
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        // Execute after delay
+        // Check NPC Progress
+        if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+        {
+            // "Collect"
+            key.gameObject.SetActive(false);
+            // Trigger Sound Effect
+            if (!key.activeSelf)
+            {
+                // Play SFX
+                // CollectedSFX.PlayOneShot(Collectible_SFX, 0.5f);
+            }
+        }
+    }
 }
